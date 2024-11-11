646829b531e9e18805bcba58378bfda89df9a43a
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-11 16:35:10 +0100

    .

---
+++ Outfit7TakeHomeTest/Data/CoreDataService.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataService.swift
+//  Outfit7TakeHomeTest
+//
+//  Created by Aleksandar Milidrag on 11. 11. 2024..
+//
+
+import Foundation
+import CoreData
+
+open class CoreDataService {
+    
+}
---
+++ Outfit7TakeHomeTest/Data/LocalEmployeeDataSourceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  LocalEmployeeDataSourceProtocol.swift
+//  Outfit7TakeHomeTest
+//
+//  Created by Aleksandar Milidrag on 11. 11. 2024.
+//
+
+import Foundation
+
+protocol LocalEmployeeDataSourceProtocol {
+    
+}
