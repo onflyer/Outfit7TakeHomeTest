1f003b1c8188dc4a9aa23d8d3d3da6f7a1bd854a
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-11 22:38:39 +0100

    added toCoreData mapper

---
+++ Outfit7TakeHomeTest/Data/DTOModels/EmployeeDomainModel+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  EmployeeDomainModel+Extension.swift
+//  Outfit7TakeHomeTest
+//
+//  Created by Aleksandar Milidrag on 11. 11. 2024..
+//
+
+import Foundation
+import CoreData
+
+extension EmployeeDomainModel {
+    
+    func toCoreDataEntity(in context: NSManagedObjectContext) -> EmployeeCoreDataEntity {
+        let employeeCoreDataEntity = EmployeeCoreDataEntity(id: id, name: name, lastName: lastName, age: age, gender: gender.rawValue, context: context)
+        employeeCoreDataEntity.id = id
+        employeeCoreDataEntity.name = name
+        employeeCoreDataEntity.lastName = lastName
+        employeeCoreDataEntity.age = age
+        employeeCoreDataEntity.gender = gender.rawValue
+        
+        return employeeCoreDataEntity
+        
+    }
+}
