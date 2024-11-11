0002bed1208400139ce2423ddb85d6219cd844c8
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-11 16:07:33 +0100

    Initial Commit

---
+++ Outfit7TakeHomeTest/Outfit7TakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Outfit7TakeHomeTestApp.swift
+//  Outfit7TakeHomeTest
+//
+//  Created by Aleksandar Milidrag on 11. 11. 2024..
+//
+
+import SwiftUI
+
+@main
+struct Outfit7TakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
