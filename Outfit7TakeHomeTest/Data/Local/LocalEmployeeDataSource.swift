e042d6dbdf487460c622655e6f8775db67d1fe8b
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-11 23:07:28 +0100

    Update LocalEmployeeDataSource.swift

--- Outfit7TakeHomeTest/Data/Local/LocalEmployeeDataSource.swift
+++ Outfit7TakeHomeTest/Data/Local/LocalEmployeeDataSource.swift
@@ -30,4 +30,33 @@ class LocalEmployeeDataSource {
             employees.toDomain()
         }
     }
+    
+    func addEmployee(employee: EmployeeDomainModel) async throws {
+       let result = employee.toCoreDataEntity(in: managedObjectContext)
+        await managedObjectContext.perform {
+            self.coreDataService.saveContext()
+        }
+    }
+    
+    func removeEmployee(employee: EmployeeDomainModel) async throws {
+        let fetchRequest = EmployeeCoreDataEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", employee.id as CVarArg)
+        let results = try managedObjectContext.fetch(fetchRequest)
+        results.forEach { managedObjectContext.delete($0) }
+        coreDataService.saveContext()
+    }
+    
+    func updateEmployee(employee: EmployeeDomainModel) async throws {
+        let fetchRequest = EmployeeCoreDataEntity.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "id == %@", employee.id as CVarArg)
+        let result = try managedObjectContext.fetch(fetchRequest)
+        result.forEach { oldEmployee in
+            oldEmployee.name = employee.name
+            oldEmployee.lastName = employee.lastName
+            oldEmployee.age = employee.age
+            oldEmployee.gender = employee.gender.rawValue
+            coreDataService.saveContext()
+        }
+        
+    }
 }
