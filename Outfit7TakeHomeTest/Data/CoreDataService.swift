a59f9abfe07cd4c12d68c1aa004643330396ae1a
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-11 17:17:40 +0100

    added CoreDataService

--- Outfit7TakeHomeTest/Data/CoreDataService.swift
+++ Outfit7TakeHomeTest/Data/CoreDataService.swift
@@ -9,5 +9,60 @@ import Foundation
 import CoreData
 
 open class CoreDataService {
+    public static let modelName = "EmployeeCoreDataModel"
+    public static let model: NSManagedObjectModel = {
+        let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!}()
     
+    public init() {}
+    
+    public lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: CoreDataService.modelName, managedObjectModel: CoreDataService.model)
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    public lazy var mainContext: NSManagedObjectContext = {
+        return self.storeContainer.viewContext
+    }()
+    
+    public func newBackgroundContext() -> NSManagedObjectContext {
+        let context = storeContainer.newBackgroundContext()
+        return context
+    }
+    
+    public func saveDerivedContext(_ context: NSManagedObjectContext) {
+        context.perform {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+            
+            self.saveContext(self.mainContext)
+        }
+    }
+    
+    public func saveContext(_ context: NSManagedObjectContext) {
+        if context != mainContext {
+            saveDerivedContext(context)
+            return
+        }
+        
+        context.perform {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+    
+    public func saveContext() {
+        saveContext(mainContext)
+    }
 }
