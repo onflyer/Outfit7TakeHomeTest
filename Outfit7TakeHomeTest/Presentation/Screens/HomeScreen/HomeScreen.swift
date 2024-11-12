cc69e73239089efef0b9f4a0479d5f652a97ec83
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-12 10:41:29 +0100

    Update HomeScreen.swift

--- Outfit7TakeHomeTest/Presentation/Screens/HomeScreen/HomeScreen.swift
+++ Outfit7TakeHomeTest/Presentation/Screens/HomeScreen/HomeScreen.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct HomeScreen: View {
+    @StateObject var viewModel = HomeScreenViewModel(
+        repository: EmployeesLocalRepository(
+            repository: LocalEmployeeDataSource(
+                coreDataService: CoreDataService())))
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
