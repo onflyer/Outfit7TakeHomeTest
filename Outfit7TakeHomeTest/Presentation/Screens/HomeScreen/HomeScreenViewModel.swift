597a7ecc76e979b574a07c6cbb83f94992d77db4
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-12 10:39:56 +0100

    Update HomeScreenViewModel.swift

--- Outfit7TakeHomeTest/Presentation/Screens/HomeScreen/HomeScreenViewModel.swift
+++ Outfit7TakeHomeTest/Presentation/Screens/HomeScreen/HomeScreenViewModel.swift
@@ -9,6 +9,12 @@ import Foundation
 
 class HomeScreenViewModel: ObservableObject {
     
+    let repository: EmployeesLocalRepository
+    
+    init(repository: EmployeesLocalRepository) {
+        self.repository = repository
+    }
+    
     @Published var state: ViewState = .idle
     @Published var employees: [EmployeeDomainModel] = []
 }
