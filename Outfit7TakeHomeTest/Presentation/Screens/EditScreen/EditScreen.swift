e98a1cd2248ec5f9de71bd85f75657e2cb17b32c
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-15 15:32:00 +0100

    Update EditScreen.swift

--- Outfit7TakeHomeTest/Presentation/Screens/EditScreen/EditScreen.swift
+++ Outfit7TakeHomeTest/Presentation/Screens/EditScreen/EditScreen.swift
@@ -11,44 +11,78 @@ struct EditScreen: View {
     
     @EnvironmentObject var viewModel: HomeScreenViewModel
     @Environment (\.dismiss) private var dismiss
+    
     let id: UUID
-        
+    
     var body: some View {
         NavigationStack {
             Form {
                 Section("Info") {
-                    TextField("Enter employee's name", text: $viewModel.name)
-                    TextField("Enter employee's last name", text: $viewModel.lastName)
-                    Picker("Select age", selection: $viewModel.age) {
-                        ForEach(18..<100) { age in
-                            Text("\(age)").tag(age)
-                        }
-                    }
-                    .pickerStyle(.menu)
-                    
-                    Picker("Gender", selection: $viewModel.gender) {
-                        ForEach(Gender.allCases) { gender in
-                            Text(gender.rawValue)
-                        }
-                    }
+                    nameTextfield
+                    lastNameTextfield
+                    agePicker
+                    genderPicker
                 }
             }
             .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button("Done") {
-                        viewModel.updateEmployee(employee: EmployeeDomainModel(id: viewModel.employee!.id, name: viewModel.name, lastName: viewModel.lastName, age: viewModel.age, gender: viewModel.gender))
-                        Task {
-                            await viewModel.fetchEmployees()
-                        }
-                        dismiss()
-                        
-                    }
-                    .disabled(!viewModel.isFormValid)
-                }
+                toolbarButtonCancel
+                toolbarButtonDone
             }
             .navigationTitle("Edit employee")
             .task {
                 viewModel.getEmployee(id: id)
+            }
+        }
+    }
+}
+
+extension EditScreen {
+    var nameTextfield: some View {
+        TextField("Enter employee's name", text: $viewModel.name)
+        
+    }
+    
+    var lastNameTextfield: some View {
+        TextField("Enter employee's last name", text: $viewModel.lastName)
+        
+    }
+    
+    var agePicker: some View {
+        Picker("Select age", selection: $viewModel.age) {
+            ForEach(18..<100) { age in
+                Text("\(age)").tag(age)
+            }
+        }
+        .pickerStyle(.menu)
+        
+    }
+    
+    var genderPicker: some View {
+        Picker("Gender", selection: $viewModel.gender) {
+            ForEach(Gender.allCases) { gender in
+                Text(gender.rawValue)
+            }
+        }
+    }
+    
+    var toolbarButtonDone: some ToolbarContent {
+        ToolbarItem(placement: .topBarTrailing) {
+            Button("Done") {
+                viewModel.updateEmployee(employee: EmployeeDomainModel(id: viewModel.employee!.id, name: viewModel.name, lastName: viewModel.lastName, age: viewModel.age, gender: viewModel.gender))
+                Task {
+                    await viewModel.fetchEmployees()
+                }
+                dismiss()
+                
+            }
+            .disabled(!viewModel.isFormValid)
+        }
+    }
+    
+    var toolbarButtonCancel: some ToolbarContent {
+        ToolbarItem(placement: .topBarTrailing) {
+            Button("Cancel") {
+                dismiss()
             }
         }
     }
