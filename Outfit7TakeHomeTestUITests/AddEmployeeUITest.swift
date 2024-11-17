016596ed1f84c93513cfafb36abe083a3a0ff12f
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-17 23:23:16 +0100

    .

---
+++ Outfit7TakeHomeTestUITests/AddEmployeeUITest.swift
@@ -0,0 +1,28 @@
+//
+//  AddEmployeeUITest.swift
+//  Outfit7TakeHomeTestUITests
+//
+//  Created by Aleksandar Milidrag on 17. 11. 2024..
+//
+
+import XCTest
+
+final class AddEmployeeUITest: XCTestCase {
+
+    private var app: XCUIApplication!
+    
+    override func setUp() {
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        app = nil
+    }
+    
+    func test_ListNavigationSelection() throws {
+        
+    }
+}
