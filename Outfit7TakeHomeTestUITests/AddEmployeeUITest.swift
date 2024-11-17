0367b8d5daf1a910f259fb113e294c6095cbd92e
Aleksandar Milidrag <salkaorama@gmail.com>
2024-11-18 00:05:47 +0100

    Update AddEmployeeUITest.swift

--- Outfit7TakeHomeTestUITests/AddEmployeeUITest.swift
+++ Outfit7TakeHomeTestUITests/AddEmployeeUITest.swift
@@ -22,7 +22,24 @@ final class AddEmployeeUITest: XCTestCase {
         app = nil
     }
     
-    func testListNavigation() throws {
+    func testAddEmployeeProcess() throws {
         
+        let app = XCUIApplication()
+        app.navigationBars["Employees"]/*@START_MENU_TOKEN@*/.buttons["Add"]/*[[".otherElements[\"Stats\"].buttons[\"Add\"]",".buttons[\"Add\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        XCTAssertTrue(app.navigationBars.buttons["Add"].exists)
+        
+        let collectionViewsQuery = app.collectionViews
+        let enterEmployeeSNameTextField = collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["Enter employee's name"]/*[[".cells.textFields[\"Enter employee's name\"]",".textFields[\"Enter employee's name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        XCTAssertTrue(enterEmployeeSNameTextField.exists)
+        enterEmployeeSNameTextField.tap()
+        enterEmployeeSNameTextField.typeText("Test")
+        
+        let enterEmployeeSLastNameTextField = collectionViewsQuery/*@START_MENU_TOKEN@*/.textFields["Enter employee's last name"]/*[[".cells.textFields[\"Enter employee's last name\"]",".textFields[\"Enter employee's last name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        XCTAssertTrue(enterEmployeeSLastNameTextField.exists)
+        enterEmployeeSLastNameTextField.tap()
+        enterEmployeeSLastNameTextField.tap()
+        enterEmployeeSLastNameTextField.typeText("Test")
+        XCTAssertTrue(app.buttons["Done"].exists)
+        app.navigationBars["Add employee"]/*@START_MENU_TOKEN@*/.buttons["Done"]/*[[".otherElements[\"Done\"].buttons[\"Done\"]",".buttons[\"Done\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
     }
 }
